6d8eaf9f8e1f3ecde43b0e8904fc0ca920663200
Oliver Zeyen <o.zeyen@de.edenspiekermann.com>
2014-02-25 15:03:24 +0100

    updated carrier wave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,12 @@
 CarrierWave.configure do |config|
-  config.storage = :fog
   config.fog_credentials = {
-    :provider              => ENV['FOG_PROVIDER'],
-    :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :provider               => ENV['FOG_PROVIDER'],                        # required
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                        # required
+    :region                 => ENV['FOG_REGION']                  # optional, defaults to 'us-east-1'
+  # optional, defaults to nil
   }
-  config.fog_directory = 'responsiveimages'
-  config.fog_region = 'eu-west-1'
+  config.fog_directory  = 'responsiveimages'                     # required
+ #config.fog_public     = false                                   # optional, defaults to true
+  #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
